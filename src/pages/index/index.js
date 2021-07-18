1827bfee18d12812a41e1b60b6707fae27cb04e1
khanhvankhanh141 <72251681+khanhvankhanh141@users.noreply.github.com>
2021-07-19 00:08:43 +0700

    7

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,7 +1,9 @@
 Page({
 
   data: {
-    fixedHeader: false
+    fixedHeader: false,
+    width: 300,
+    height: 300
   },
   onPageScroll(event) {
     this.setData({ fixedHeader: event.scrollTop > 20 });
@@ -9,7 +11,15 @@ Page({
   onTapCart() {
     console.log('onTapCart');
   },
-
+  onLoad() {
+    my.getSystemInfo({
+      success: (systemInfo) => {
+        console.log('width :>> ', JSON.stringify(systemInfo));
+        console.log('height :>> ', systemInfo.windowHeight);
+        // this.setData({ width, height });
+      },
+    });
+  },
   onLoad(query) {
   },
   onReady() {
